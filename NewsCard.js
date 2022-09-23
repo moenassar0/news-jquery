317eb54f560931fa7c06f531b8cc7cd73351b0aa
Mohamad Nassar <mnassar57@gmail.com>
2022-09-23 12:23:17 +0300

    cards are added to page

--- NewsCard.js
+++ NewsCard.js
@@ -1,5 +1,12 @@
 export const NewsCard = (newscard) => {
     return (`
-        
+    <div class="card" style="width: 18rem;">
+    <img src="${newscard.urlToImage}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${newscard.title}</h5>
+      <p class="card-text">${newscard.description}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
     `)
 }
