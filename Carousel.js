fb6ddb18516a8436478e35caa5d0ff24e4497b11
Mohamad Nassar <mnassar57@gmail.com>
2022-09-23 12:51:40 +0300

    adding carousel to site

---
+++ Carousel.js
@@ -0,0 +1,9 @@
+
+
+export const Carousel = (carousel) => {
+    return (`
+    <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+    </div>
+    `)
+}
--- index.js
+++ index.js
@@ -1,58 +1,47 @@
 const container = document.getElementById("container");
-
-import { NewsCard } from "./NewsCard.js";
-
-
+const carousel = document.getElementById("carousel-inner");
 const url = "./getnews.php";
 
-let data = {};
+import { NewsCard } from "./NewsCard.js";
+import { Carousel } from "./Carousel.js";
 
-    async function doAjax(args) {
-        let result;
-    
-        try {
-            result = await $.ajax({
-                url: url,
-                type: 'GET',
-                data: args
-            });
-    
-            return result;
-        } catch (error) {
-            console.error(error);
-        }
+async function doAjax(args) {
+    let result;
+
+    try {
+        result = await $.ajax({
+            url: url,
+            type: 'GET',
+            data: args
+        });
+
+        return result;
+    } catch (error) {
+        console.error(error);
     }
+}
 
-    async function addArticle(stuff){
-        let nRows = stuff.length / 3;
-        nRows = (Math.ceil(nRows));
-        for(let i = 0; i < nRows; i++){
-            let row = document.createElement('div');
-            row.classList.add("row");
-            container.appendChild(row);
-            for(let x = 0; x < 3; x++){
-                let col = document.createElement('div');
-                col.classList.add("col");
-                row.appendChild(col);
-            }
-        }   
-        let cols = document.getElementsByClassName('col');
-            let HTML = '';
-            stuff.map((newscard, i) => {
-                cols[i].innerHTML = NewsCard(newscard);
-                //console.log(`${newscard.description}`);
-            })
-            //document.getElementById("container").innerHTML += HTML;
-
-        }
-        /*let row = document.createElement('div');
+async function addArticle(stuff){
+    let nRows = stuff.length / 3;
+    nRows = (Math.ceil(nRows));
+    for(let i = 0; i < nRows; i++){
+        let row = document.createElement('div');
         row.classList.add("row");
-        let col = document.createElement('div');
-        col.classList.add('col');
-        console.log(data[0])
-        col.innerHTML = data[0].description;
-        row.appendChild(col);
-        container.append(row);*/
-    //}
+        container.appendChild(row);
+        for(let x = 0; x < 3; x++){
+            let col = document.createElement('div');
+            col.classList.add("col");
+            row.appendChild(col);
+        }
+    }   
+    let cols = document.getElementsByClassName('col');
+        let HTML = '';
+        stuff.map((newscard, i) => {
+            cols[i].innerHTML = NewsCard(newscard);
+            //console.log(`${newscard.description}`);
+        })
+        //document.getElementById("container").innerHTML += HTML;
 
-    doAjax({}).then( (data) => addArticle(data) )
+}
+
+doAjax({}).then( (data) => addArticle(data) )
