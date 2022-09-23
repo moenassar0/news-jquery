a2aec4b8a1c8444f2c9b3a026cf00803ec1934b1
Mohamad Nassar <mnassar57@gmail.com>
2022-09-23 11:08:39 +0300

    database connection

---
+++ getnews.php
@@ -0,0 +1,9 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    include("connection.php");
+    
+    if(isset($_POST['']) && isset($_POST[''])){
+    }
+?>
