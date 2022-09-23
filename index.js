b9513f819e85017c60266da3b66a20acbdfef9cf
Mohamad Nassar <mnassar57@gmail.com>
2022-09-23 12:37:48 +0300

    cards now instantiate from database

--- index.js
+++ index.js
@@ -7,29 +7,6 @@ const url = "./getnews.php";
 
 let data = {};
 
-//addData(data, url);
-
-/*function addData(data){// pass your data in method
-     $.ajax({
-             type: "GET",
-             url: url,
-             data: JSON.stringify(data),// now data come in this function
-             contentType: "application/json; charset=utf-8",
-             crossDomain: true,
-             dataType: "json",
-             success: function (data, status, jqXHR) {
-
-                addArticle(data);// write success in " "
-             },
-
-             error: function (jqXHR, status) {
-                 // error handler
-                 console.log(jqXHR);
-                 alert('fail' + status.code);
-             }
-          });
-    }*/
-
     async function doAjax(args) {
         let result;
     
@@ -47,26 +24,25 @@ let data = {};
     }
 
     async function addArticle(stuff){
-        //const stuff = await doAjax({});
-        //console.log(stuff[0])
-        //for(let i = 0; i < stuff.length; i++){
-            //return 
-            /*
-                        <div class="card" style="width: 18rem;">
-              <img src="" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            */
+        let nRows = stuff.length / 3;
+        nRows = (Math.ceil(nRows));
+        for(let i = 0; i < nRows; i++){
+            let row = document.createElement('div');
+            row.classList.add("row");
+            container.appendChild(row);
+            for(let x = 0; x < 3; x++){
+                let col = document.createElement('div');
+                col.classList.add("col");
+                row.appendChild(col);
+            }
+        }   
+        let cols = document.getElementsByClassName('col');
             let HTML = '';
-            stuff.map(newscard => {
-                HTML += NewsCard(newscard);
+            stuff.map((newscard, i) => {
+                cols[i].innerHTML = NewsCard(newscard);
                 //console.log(`${newscard.description}`);
             })
-            document.getElementById("container").innerHTML = HTML;
+            //document.getElementById("container").innerHTML += HTML;
 
         }
         /*let row = document.createElement('div');
